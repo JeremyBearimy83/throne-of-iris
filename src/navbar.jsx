d949d6a2e39d7fd4dd6b66d922e660d31946452e
Ujjwal Singhal <ujjwals@gmail.com>
2020-07-10 18:33:36 +0530

    Add snackbar component, use material icons, style select component

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,25 +2,30 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./navbar.css";
-import { Select, MenuItem } from "@material-ui/core/";
+import { Select, MenuItem, Icon } from "@material-ui/core/";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { format: this.props.format };
+    this.state = { format: this.props.format, open: false };
     this.handleFormat = this.handleFormat.bind(this);
+    this.changeSnackbar = this.changeSnackbar.bind(this);
+  }
+  changeSnackbar() {
+    this.setState({ open: false });
   }
   handleFormat(e) {
-    console.log(
-      "THOU HAVE ENTERED HANDLE FORMAT IN NAVBAR THIS WILL CALL HANDLE FORMAT IN PALETTE"
-    );
-    this.setState({ format: e.target.value });
+    this.setState({ format: e.target.value, open: true });
     this.props.handleFormat(e.target.value);
   }
   render() {
     return (
       <div className="Navbar">
         <div className="brand">
-          <span>Throne/OfIris</span>
+          <span>Throne/Of/Iris</span>
         </div>
         <div className="slider-container">
           <span>{`Shade: ${this.props.shade}`}</span>
@@ -41,6 +46,18 @@ class Navbar extends Component {
             <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1,0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          message={`Format changed to ${this.state.format}`}
+          open={this.state.open}
+          onClose={this.changeSnackbar}
+          action={
+            <IconButton color="inherit" onClick={this.changeSnackbar}>
+              <CloseIcon />
+            </IconButton>
+          }
+          autoHideDuration={3000}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        />
       </div>
     );
   }
