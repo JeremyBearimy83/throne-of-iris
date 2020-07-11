35ae38921768011d33808837d7e8f0d64bcbc820
Ujjwal Singhal <ujjwals@gmail.com>
2020-07-11 12:46:06 +0530

    Integrate react-router-dom in project, add a palette list route and pallete/id route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Palette from "./palette.jsx";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import defaultPaletteData from "./default-palette-data";
 import { generatePalette } from "./color-shades-generator";
@@ -7,7 +8,14 @@ function App() {
   console.log(generatePalette(defaultPaletteData[8]));
   return (
     <div className="App">
-      <Palette palette={generatePalette(defaultPaletteData[8])} />
+      <Switch>
+        <Route exact path="/">
+          <h1>Palette List</h1>
+        </Route>
+        <Route path="/palette/:id">
+          <Palette palette={generatePalette(defaultPaletteData[8])} />
+        </Route>
+      </Switch>
     </div>
   );
 }
