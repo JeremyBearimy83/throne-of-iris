6becb33146fc27f832d00cab02db0154e330a4df
Ujjwal Singhal <ujjwals@gmail.com>
2020-07-11 13:31:42 +0530

    Configure Palette route to give correct route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Palette from "./palette.jsx";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import defaultPaletteData from "./default-palette-data";
 import { generatePalette } from "./color-shades-generator";
-function App() {
-  console.log(generatePalette(defaultPaletteData[8]));
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <h1>Palette List</h1>
-        </Route>
-        <Route path="/palette/:id">
-          <Palette palette={generatePalette(defaultPaletteData[8])} />
-        </Route>
-      </Switch>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.getPalette = this.getPalette.bind(this);
+  }
+  getPalette(props) {
+    const id = props.match.params.id;
+    const currentPalette = defaultPaletteData.find((el) => el.id === id);
+    return <Palette palette={generatePalette(currentPalette)} />;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <h1>Palette List</h1>
+          </Route>
+          <Route path="/palette/:id" render={this.getPalette}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
