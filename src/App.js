39ff615b3b22185de9e2977dd67a041de25a0909
Ujjwal Singhal <ujjwals@gmail.com>
2020-07-13 13:27:12 +0530

    Create single color palette route and component, link them, provide necessary props and create a TodayTodo text file

--- src/App.js
+++ src/App.js
@@ -5,16 +5,54 @@ import "./App.css";
 import defaultPaletteData from "./default-palette-data";
 import { generatePalette } from "./color-shades-generator";
 import PaletteList from "./palette-list.jsx";
+import SingleColorPalette from "./single-color-palette.jsx";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.getPalette = this.getPalette.bind(this);
   }
-  getPalette(props) {
+  getColor(props) {
+    const colorName = props.match.params.colorName;
     const id = props.match.params.id;
     const currentPalette = defaultPaletteData.find((el) => el.id === id);
-    return <Palette palette={generatePalette(currentPalette)} />;
+    const paletteData = generatePalette(currentPalette);
+    let firstColor = {};
+    const singleColor = [];
+    let bruh;
+    for (let i = 0; i < 20; i++) {
+      firstColor = {};
+      for (const shade in paletteData.colors) {
+        bruh = paletteData.colors[shade];
+        firstColor[shade] = bruh[i];
+      }
+      singleColor.push(firstColor);
+    }
+    const currentColor = singleColor.find((el) => el[50].id === colorName);
+    return <SingleColorPalette {...currentColor} />;
+  }
+  getPalette(props) {
+    let id = props.match.params.id;
+    const currentPalette = defaultPaletteData.find((el) => el.id === id);
+    const paletteData = generatePalette(currentPalette);
+    let firstColor = {};
+    const singleColor = [];
+    let bruh;
+    for (let i = 0; i < 20; i++) {
+      firstColor = {};
+      for (const shade in paletteData.colors) {
+        bruh = paletteData.colors[shade];
+        firstColor[shade] = bruh[i];
+      }
+      singleColor.push(firstColor);
+    }
+    const currentColor = singleColor.find((el) => el[50].id === "turquoise");
+    console.log(currentColor);
+    console.log("--------LOOK HERE --------AS WELL ----");
+    console.log(singleColor);
+    console.log("---------LOOK HERE ------ LOOK HERE----");
+    console.log(paletteData);
+    return <Palette palette={paletteData} />;
   }
 
   render() {
@@ -27,7 +65,12 @@ class App extends Component {
             render={(routeProps) => <PaletteList {...routeProps} />}
           />
 
-          <Route path="/palette/:id" render={this.getPalette}></Route>
+          <Route exact path="/palette/:id" render={this.getPalette}></Route>
+          <Route
+            exact
+            path="/palette/:id/match/:colorName"
+            render={this.getColor}
+          ></Route>
         </Switch>
       </div>
     );
---
+++ src/single-color-palette.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+class SingleColorPalette extends Component {
+  render() {
+    return <h1>lol</h1>;
+  }
+}
+
+export default SingleColorPalette;
