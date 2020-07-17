37d293253ab4575e1aead912e233635c138e3e79
Ujjwal Singhal <ujjwals@gmail.com>
2020-07-17 12:09:39 +0530

    Palette styles refactored

--- src/palette.jsx
+++ src/palette.jsx
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 import ColorBox from "./color-box";
 import "./palette.css";
 import Navbar from "./navbar";
+import { withStyles } from "@material-ui/styles";
+const styles = {
+  palette: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    overflow: "hidden",
+  },
+  colorBoxContainer: {
+    height: "90%",
+  },
+  footer: {
+    height: "5vh",
+    display: "flex",
+    justifyContent: "flex-end",
+    fontSize: "17px",
+    alignItems: "center",
+    marginRight: "10px",
+  },
+  icon: {
+    margin: "0px 7px",
+    fontSize: "1.5rem",
+  },
+};
 class Palette extends Component {
   constructor(props) {
     super(props);
@@ -16,6 +40,7 @@ class Palette extends Component {
     this.setState({ shade: shade });
   }
   render() {
+    const { classes } = this.props;
     const colorBoxes = this.props.palette.colors[
       this.state.shade
     ].map((color) => (
@@ -26,7 +51,7 @@ class Palette extends Component {
       />
     ));
     return (
-      <div className="Palette">
+      <div className={classes.palette}>
         <Navbar
           shade={this.state.shade}
           handleShade={this.changeShade}
@@ -34,13 +59,13 @@ class Palette extends Component {
           handleFormat={this.changeFormat}
         />
         {/* Navbar goes here */}
-        <div className="color-box-container">{colorBoxes}</div>
-        <footer className="footer">
+        <div className={classes.colorBoxContainer}>{colorBoxes}</div>
+        <footer className={classes.footer}>
           {this.props.palette.paletteName}
-          <span className="icon">{this.props.palette.emoji}</span>
+          <span className={classes.icon}>{this.props.palette.emoji}</span>
         </footer>
       </div>
     );
   }
 }
-export default Palette;
+export default withStyles(styles)(Palette);
