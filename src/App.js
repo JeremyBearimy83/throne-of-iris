dadb5db7a883e0203a340f167ec50e326307933e
Ujjwal Singhal <ujjwals@gmail.com>
2020-08-02 16:35:49 +0530

    Checking remote connection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+//IS GIT WORKING?
 import Palette from "./palette.jsx";
 import { Route, Switch } from "react-router-dom";
 import defaultPaletteData from "./default-palette-data";
